594c2ba7133310fcbeb927557c628f9b9c272e14
tannerwride <tanner@circleci.com>
2023-06-28 12:34:52 -0600

    added home page

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ app.get('/',  (req, res) => {
     res.send('Hello TSMs')
 });
 
+app.use(express.static('public'))
+
 app.listen(3000,  () => {
     console.log('Example app listening on port 3000')
 });
