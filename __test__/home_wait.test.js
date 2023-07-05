1fa45201fdb3b1c1c85fa2d92b3ff86f52ed3130
tannerwride <tanner@circleci.com>
2023-07-05 12:23:05 -0600

    updated test times

--- __test__/home_wait.test.js
+++ __test__/home_wait.test.js
@@ -1,8 +1,8 @@
-jest.setTimeout(30000);
+jest.setTimeout(50000);
 describe('Time test', () => {
     test('Timed test #1', async () => {
         const foo = true;
-        await new Promise((r) => setTimeout(r, 20000));
+        await new Promise((r) => setTimeout(r, 30000));
         expect(foo).toBeDefined();
       });
 })
--- __test__/page3_wait.test.js
+++ __test__/page3_wait.test.js
@@ -2,7 +2,7 @@ jest.setTimeout(30000);
 describe('Time test', () => {
     test('Timed test #4', async () => {
         const foo = true;
-        await new Promise((r) => setTimeout(r, 20000));
+        await new Promise((r) => setTimeout(r, 40000));
         expect(foo).toBeDefined();
       });
 })
