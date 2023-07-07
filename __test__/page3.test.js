6c00e80de4d1733d2888705f2f1ab24916f7c704
tannerwride <tanner@circleci.com>
2023-07-07 13:07:35 -0600

    update

---
+++ __test__/page3.test.js
@@ -0,0 +1,54 @@
+const request = require('supertest');
+const app = require('../app'); 
+jest.setTimeout(300000);
+describe("Test the root path", () => {
+    test("page3 test", done => {
+      request(app)
+        .get("/page3.html")
+        .then(response => {
+          expect(response.statusCode).toBe(200);
+          done();
+        });
+    });
+  });
+
+describe('Time test', () => {
+  test('page 3 timed', async () => {
+      const foo = true;
+      await new Promise((r) => setTimeout(r, 100000));
+      expect(foo).toBeDefined();
+    });
+})
+
+describe("Test the root path", () => {
+  test("page3 test 1", done => {
+    request(app)
+      .get("/page3.html")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
+
+describe("Test the root path", () => {
+  test("page3 test 2", done => {
+    request(app)
+      .get("/page3.html")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
+
+describe("Test the root path", () => {
+  test("page3 test 3", done => {
+    request(app)
+      .get("/page3.html")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express() 
+
+
+app.get('/',  (req, res) => {
+    res.status(200).send('Hello TSMs')
+});
+
+app.use(express.static('public'))
+
+module.exports = app;
+
