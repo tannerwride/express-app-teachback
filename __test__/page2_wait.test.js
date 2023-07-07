262ccb8545f4fab932b9513458d422fb4d9e1c05
tannerwride <tanner@circleci.com>
2023-07-07 12:50:24 -0600

    added images

--- __test__/page2_wait.test.js
+++
@@ -1,9 +0,0 @@
-jest.setTimeout(300000);
-describe('Time test', () => {
-    test('Timed test #3', async () => {
-        const foo = true;
-        await new Promise((r) => setTimeout(r, 20000));
-        expect(foo).toBeDefined();
-      });
-})
-
--- __test__/page3_wait.test.js
+++
@@ -1,8 +0,0 @@
-jest.setTimeout(500000);
-describe('Time test', () => {
-    test('Timed test #4', async () => {
-        const foo = true;
-        await new Promise((r) => setTimeout(r, 60000));
-        expect(foo).toBeDefined();
-      });
-})
